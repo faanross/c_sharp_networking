03b9fcdec8dc646cacf1a5691dcdd90f8e307383
faanross <moi@faanross.com>
2024-02-01 07:58:55 -0500

    .

--- simple_ip_address_example/obj/Debug/net8.0/simple_ip_address_example.AssemblyInfo.cs
+++ simple_ip_address_example/obj/Debug/net8.0/simple_ip_address_example.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("simple_ip_address_example")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82353604fbcf27c3e5e11de83a1e0c134f011d3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b77069d92bb648d528c596748f150add533f38d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("simple_ip_address_example")]
 [assembly: System.Reflection.AssemblyTitleAttribute("simple_ip_address_example")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
